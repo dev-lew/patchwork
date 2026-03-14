88f6e78c7e56bfa546d33b3f1a3ddab05a6db4f0
devlew <dev.lewis611@gmail.com>
2026-03-14 17:27:36 -0400

    Add auth to protect some endpoints

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,18 +1,23 @@
 import logging
 import os
+import secrets
 import sys
 from collections.abc import Sequence
 from typing import Annotated
 
-from fastapi import Depends, FastAPI, Query
+from fastapi import Depends, FastAPI, HTTPException, Query
+from fastapi.security import APIKeyHeader
 from sqlalchemy.exc import MultipleResultsFound
 from sqlmodel import Session, create_engine, select
 
 from app.enums import Category
 from app.models import Product
 
-if (postgres_url := os.getenv("POSTGRES_URL")) is None:
+if (POSTGRES_URL := os.getenv("POSTGRES_URL")) is None:
     sys.exit("The POSTGRES_URL env variable must be set.")
+
+if (API_KEY := os.getenv("API_KEY")) is None:
+    sys.exit("The API_KEY env variable must be set.")
 
 logging.basicConfig(
     format="%(asctime)s %(levelname)s [%(name)s] %(message)s",
@@ -20,7 +25,7 @@ logging.basicConfig(
 
 logger = logging.getLogger(__name__)
 
-engine = create_engine(postgres_url)
+engine = create_engine(POSTGRES_URL)
 
 
 def get_session():
@@ -29,6 +34,12 @@ def get_session():
 
 
 SessionDep = Annotated[Session, Depends(get_session)]
+
+
+def auth_api_key(api_key: Annotated[str, Depends(APIKeyHeader(name="api-key"))]):
+    if not secrets.compare_digest(api_key, API_KEY):
+        raise HTTPException(status_code=403, detail="Invalid API key")
+
 
 app = FastAPI()
 
